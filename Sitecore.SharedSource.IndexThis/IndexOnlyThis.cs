0082d91577a5bf2a079f5ddf6d40925296f8defc
Mortaza Kamal Nourestani <mkn@dodana.com>
2014-06-04 15:08:27 -0400

    initial commit

---
+++ Sitecore.SharedSource.IndexThis/IndexOnlyThis.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.SharedSource.IndexThis
+{
+    using Data.Items;
+    using Diagnostics;
+    using Shell.Framework.Commands;
+
+    internal class IndexOnlyThis : Command
+    {
+
+        /// <summary>
+        /// Executes the command in the specified context.
+        /// </summary>
+        /// <param name="commandContext">The command context.</param>
+        public override void Execute(CommandContext commandContext)
+        {
+            Assert.ArgumentNotNull(commandContext, "commandContext");
+
+            if (commandContext.Items.Length >= 1)
+            {
+                Item item = commandContext.Items[0];
+
+                Log.Info(string.Format("About to run Index This item: '{0}'.", item.Paths.FullPath), this);
+
+                if (item != null)
+                    item.Database.Engines.HistoryEngine.RegisterItemSaved(item, null);
+            }
+        }
+
+    }
+}
---
+++ Sitecore.SharedSource.IndexThis/IndexThisAndDescendants.cs
@@ -0,0 +1,41 @@
+﻿namespace Sitecore.SharedSource.IndexThis
+{
+    using System.Linq;
+    using Data.Items;
+    using Diagnostics;
+    using Shell.Framework.Commands;
+
+    internal class IndexThisAndDescendants : Command
+    {
+        /// <summary>
+        /// Executes the command in the specified context.
+        /// </summary>
+        /// <param name="commandContext">The command context.</param>
+        public override void Execute(CommandContext commandContext)
+        {
+            Assert.ArgumentNotNull(commandContext, "commandContext");
+
+            if (commandContext.Items.Length >= 1)
+            {
+                Item item = commandContext.Items[0];
+
+                if (item != null)
+                {
+                    Item[] descendants = item.Axes.GetDescendants();
+
+                    Log.Info(string.Format("About to run Index This And Descendants of item: '{0}'.", item.Paths.FullPath), this);
+
+                    item.Database.Engines.HistoryEngine.RegisterItemSaved(item, null);
+
+                    if (descendants != null && descendants.Any())
+                    {
+                        foreach (Item descendant in descendants)
+                        {
+                            descendant.Database.Engines.HistoryEngine.RegisterItemSaved(descendant, null);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
